6931ad6c301fc2051c51ec7eba10446f73ba0e6c
Laura Mella <laura.e.mella@student.jyu.fi>
2022-08-13 09:08:42 +0300

    eslint

--- osa4/blogilista/app.js
+++ osa4/blogilista/app.js
@@ -10,17 +10,16 @@ const mongoose = require('mongoose')
 console.log('connecting to', config.MONGODB_URI)
 
 mongoose.connect(config.MONGODB_URI)
-  .then(result => {
+  .then(() => {
     logger.info('connected to MongoDB')
   })
   .catch((error) => {
     logger.error('error connecting to MongoDB:', error.message)
-})
+  })
 
 app.use(cors())
 //app.use(express.static('build'))
 app.use(express.json())
-//app.use(middleware.requestLogger)
 
 app.use('/api/blogs', blogsRouter)
 
