d06745a43d2ec0c0e6098e9f20a4583ac25727e9
Laura Mella <laura.e.mella@student.jyu.fi>
2022-08-24 22:47:29 +0300

    some tests fixed

--- osa4/blogilista/tests/blogilist_api.test.js
+++ osa4/blogilista/tests/blogilist_api.test.js
@@ -5,16 +5,20 @@ const app = require('../app')
 const api = supertest(app)
 
 const Blog = require('../models/blog')
+const User = require('../models/user')
+const bcrypt = require('bcrypt')
+const jwt = require('jsonwebtoken')
 
 beforeEach(async () => {
   await Blog.deleteMany({})
+  await Blog.insertMany(helper.initialBlogs)
 
-  const blogObjects = helper.initialBlogs
-    .map(blog => new Blog(blog))
-  const promiseArray = blogObjects.map(blog => blog.save())
-  await Promise.all(promiseArray)
+  await User.deleteMany({})
+  const password = await bcrypt.hash('salainen', 10)
+  await new User({ username: 'newuser', name: 'Aku Ankka', password }).save()
 })
 
+//npm test -- -t 'blogs are returned as json'
 test('blogs are returned as json', async () => {
   await api
     .get('/api/blogs')
@@ -33,7 +37,13 @@ test('blog identifier defined as id', async () => {
   response.body.forEach((blog) => expect(blog.id).toBeDefined())
 })
 
-test('a valid blog can be added ', async () => {
+
+test('a valid blog can be added', async () => {
+  const users = await helper.usersInDb()
+  const user = users[0]
+  const tokenUser = { username: user.username, id: user.id }
+  const token = jwt.sign(tokenUser, process.env.SECRET)
+
   const newBlog = {
     title: 'async/await simplifies making async calls',
     author: 'Test Author',
@@ -43,6 +53,7 @@ test('a valid blog can be added ', async () => {
 
   await api
     .post('/api/blogs')
+    .set('Authorization', `bearer ${token}`)
     .send(newBlog)
     .expect(201)
     .expect('Content-Type', /application\/json/)
@@ -57,7 +68,29 @@ test('a valid blog can be added ', async () => {
   )
 })
 
+test('a valid blog cannot be added by unauthorized user', async () => {
+  const newBlog = {
+    title: 'no authorization',
+    author: 'Test Author',
+    url: 'https://reactpatterns.com/',
+    likes: 4
+  }
+  await api
+    .post('/api/blogs')
+    .set('Authorization', `Bearer ${null}`)
+    .send(newBlog)
+    .expect(401)
+
+  const blogsAtEnd = await helper.blogsInDb()
+  expect(blogsAtEnd).toHaveLength(helper.initialBlogs.length)
+})
+
 test('blog gets 0 likes if there is no value', async () => {
+  const users = await helper.usersInDb()
+  const user = users[0]
+  const tokenUser = { username: user.username, id: user.id }
+  const token = jwt.sign(tokenUser, process.env.SECRET)
+
   const newBlog = {
     title: 'Field likes is not defined',
     author: 'New Author',
@@ -66,6 +99,7 @@ test('blog gets 0 likes if there is no value', async () => {
 
   await api
     .post('/api/blogs')
+    .set('Authorization', `bearer ${token}`)
     .send(newBlog)
     .expect(201)
     .expect('Content-Type', /application\/json/)
@@ -75,6 +109,7 @@ test('blog gets 0 likes if there is no value', async () => {
 
 })
 
+//FAILURE 1/3
 test('blog does not contain title', async () => {
   const newBlog = {
     author: 'New Author',
@@ -84,6 +119,7 @@ test('blog does not contain title', async () => {
   await api.post('/api/blogs').send(newBlog).expect(400)
 })
 
+//FAILURE 2/3
 test('blog does not contain url', async () => {
   const newBlog = {
     title: 'Blog without url',
@@ -94,6 +130,7 @@ test('blog does not contain url', async () => {
   await api.post('/api/blogs').send(newBlog).expect(400)
 })
 
+//FAILURE 3/3
 test('a blog can be deleted', async () => {
   const blogsAtStart = await helper.blogsInDb()
   const blogToDelete = blogsAtStart[0]
